4cc0002eea623953501aae68ee8996bee15384c7
yyellen <yyopenfolder@gmail.com>
2023-02-07 15:36:28 +0800

    feat: add a footer for layout

---
+++ src/components/Footer/index.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="sticky bottom-0 h-12 w-full flex items-center px-12 bg-gray-600 text-[#f0f0f0] shadow-lg">
+      This is a footer.
+    </footer>
+  );
+}
+
+export default Footer;
